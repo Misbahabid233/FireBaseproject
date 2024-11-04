720235557f0720e5cad6f217f22b25a1d06b072d
Misbahabid233 <misbah154233@gmail.com>
2024-11-04 10:49:47 +0500

    updation

--- profile.js
+++ profile.js
@@ -1,14 +1,22 @@
-import { getAuth ,  onAuthStateChanged } from "./firebase.js";
+import { getAuth ,  onAuthStateChanged , updateProfile } from "./firebase.js";
 getAuth()
 const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
     if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
+      
       const uid = user.uid;
       console.log(uid);
-      // ...
+      
     } else {
       console.log("User is signed out");
     }
   });
+ var userInput = prompt("Enter your name")
+  updateProfile(auth.currentUser, {
+    displayName: userInput, photoURL: "https://example.com/jane-q-user/profile.jpg"
+  }).then(() => {
+   
+  }).catch((error) => {
+   console.log(error);
+  });
+  
